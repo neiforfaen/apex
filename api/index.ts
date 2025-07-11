717e10f8bcd871c5aadaafaa2faf3de86c2508b5
Kaiden Riley <kaiden.riley@pleo.io>
2025-07-11 23:56:32 +0100

    feat: add cors middleware

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 import { handle } from 'hono/vercel'
 import type { Bindings } from './lib/types'
 
@@ -6,7 +7,7 @@ export const config = {
   runtime: 'edge',
 }
 
-const app = new Hono<Bindings>()
+const app = new Hono<Bindings>({ strict: false }).use(cors({ origin: '*' }))
 
 app.get('/', (c) => {
   return c.text('@neiforfaen/apex | Root')
